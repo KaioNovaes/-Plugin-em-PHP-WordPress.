e9a737a26b511854f5c7e7f34e94a0dc2d34b04c
Kaio Novaes <64172325+KaioNovaes@users.noreply.github.com>
2024-10-06 13:40:38 -0300

    update all

--- wp-content/themes/donovan/index.php
+++ wp-content/themes/donovan/index.php
@@ -2,6 +2,11 @@
 /**
  * The main template file
  *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package Donovan
@@ -13,17 +18,9 @@ get_header(); ?>
     <main id="main" class="site-main" role="main">
 
         <?php
-        // Define os argumentos para a consulta
-        $args = array(
-            'post_type' => array('post', 'cakes'), // Inclui os tipos de post 'post' e 'cakes'
-            'posts_per_page' => -1, // -1 para pegar todos os posts
-        );
+        if ( have_posts() ) :
 
-        $query = new WP_Query($args);
-
-        if ($query->have_posts()) :
-
-            while ($query->have_posts()) : $query->the_post();
+            while ( have_posts() ) : the_post();
                 ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                     <header class="entry-header">
@@ -70,14 +67,11 @@ get_header(); ?>
 
             <?php endwhile;
 
-            donovan_pagination(); // Mantenha isso se você quiser paginação
+            donovan_pagination();
 
         else :
-            get_template_part('template-parts/content', 'none');
-        endif;
-
-        wp_reset_postdata();
-        ?>
+            get_template_part( 'template-parts/content', 'none' );
+        endif; ?>
 
     </main><!-- #main -->
 </div><!-- #primary -->
